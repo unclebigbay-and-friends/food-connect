4fb469c8a256e545062563ff40c03fac9799a091
Oge Obubu <ogeobubu@gmail.com>
2021-10-14 13:13:10 +0100

    added sign in and sign up modal

--- main.js
+++ main.js
@@ -40,3 +40,30 @@ menu_btn.addEventListener("click", function () {
   menu_btn.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
 });
+
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  this.classList.add("active");
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => {
+    item.classList.remove("show");
+  });
+}
+
+tabItems.forEach((item) => {
+  item.addEventListener("click", selectItem);
+});
