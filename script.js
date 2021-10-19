575e4a27d32754d497ac586e2c740013a28481e1
Oge Obubu <ogeobubu@gmail.com>
2021-10-19 21:30:08 +0100

    fix overflow bug

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const header = document.querySelector(".header");
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 const loginButton = document.querySelector("#loginButton");
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 const submitForm = document.querySelector("#submitForm");
 const alert = document.querySelector(".alert");
 const closeAlert = document.querySelector(".close-btn");
 const showTheAlert = document.querySelector(".showTheAlert");
+const searchButton = document.querySelector(".searchButton");
 
 window.onload = () => {
   document.querySelector(".preloader").style.display = "none";
@@ -42,16 +47,12 @@ closeAlert.addEventListener("click", () => {
   alert.classList.remove("showTheAlert");
 });
 
-const menu_btn = document.querySelector(".hamburger");
-const mobile_menu = document.querySelector(".mobile-nav");
-
 menu_btn.addEventListener("click", function () {
   menu_btn.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
 });
 
-const tabItems = document.querySelectorAll(".tab-item");
-const tabContentItems = document.querySelectorAll(".tab-content-item");
+// Function to change between Login and Create Account
 
 function selectItem(e) {
   removeBorder();
@@ -76,3 +77,5 @@ function removeShow() {
 tabItems.forEach((item) => {
   item.addEventListener("click", selectItem);
 });
+
+// MAP FUNCTIONALITY
